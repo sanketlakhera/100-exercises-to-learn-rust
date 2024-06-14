de56012d5a2114c87cb30e6b0555ce539dc1845e
Sanket <lakhera.sanket850@gmail.com>
2024-06-14 21:14:55 +0530

    feat:
    section 3 complete

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -6,6 +6,66 @@
 //   Order must include a method named `total` that returns the total price of the order.
 //   Order must provide setters and getters for each field.
 //
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Order {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("Product name must not be empty and must be less than 300 bytes.");
+        }
+        if quantity == 0 {
+            panic!("Quantity must be strictly greater than zero.");
+        }
+        if unit_price == 0 {
+            panic!("Unit price must be strictly greater than zero.");
+        }
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        (self.quantity * self.unit_price)
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("Product name must not be empty and must be less than 300 bytes.");
+        }
+        self.product_name = product_name;
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn set_quantity(&mut self, quantity: u32) {
+        if quantity == 0 {
+            panic!("Quantity must be strictly greater than zero.");
+        }
+        self.quantity = quantity;
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        if unit_price == 0 {
+            panic!("Unit price must be strictly greater than zero.");
+        }
+        self.unit_price = unit_price;
+    }
+}
+
 // Tests are located in a different place this time—in the `tests` folder.
 // The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
 // Integration here has a very specific meaning: they test **the public API** of your project.
